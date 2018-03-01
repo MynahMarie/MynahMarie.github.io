50b3d5e0cc2baed48bcfdcc6416b860ca57f672c
MynahMarie <mynah.marie@gmail.com>
2018-03-01 03:01:55 +0200

    Initial portfolio site set up

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
+var i = 0;
+var txt = 'My name is Mynah and I\'m a Fullstack Web Developper.';
+var speed = 50;
 
+function typeWriter() {
+  console.log('inside typeWriter');
+  if (i < txt.length) {
+    document.getElementById("typeWriter").textContent += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    i = 0;
+  }
+}
 
+setTimeout(typeWriter, 1000);
+
+document.getElementById('splashIntro').addEventListener('click', function() {
+  document.getElementById('typeWriter').textContent = "";
+  setTimeout(typeWriter, 1000);
 });
