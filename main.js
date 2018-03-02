62d0b30999d7a6a12193a5748d19a500a9d2d965
MynahMarie <mynah.marie@gmail.com>
2018-03-02 02:06:46 +0200

    Styling for mobile

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ var txt = 'My name is Mynah and I\'m a Fullstack Web Developper.';
 var speed = 50;
 
 function typeWriter() {
-  console.log('inside typeWriter');
   if (i < txt.length) {
     document.getElementById("typeWriter").textContent += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   } else {
     i = 0;
+    document.getElementById('splashIntro').setAttribute('onclick') = typeWriter;
   }
 }
 
-setTimeout(typeWriter, 1000);
+setTimeout(typeWriter, 500);
 
 document.getElementById('splashIntro').addEventListener('click', function() {
+  i = 0;
   document.getElementById('typeWriter').textContent = "";
-  setTimeout(typeWriter, 1000);
+  setTimeout(typeWriter, 500);
 });
